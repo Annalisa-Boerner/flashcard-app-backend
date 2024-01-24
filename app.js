276997b8f261fd3e6dfa66e45cd7b96fba9489aa
Annalisa Boerner <annalisa.boerner@gmail.com>
2024-01-24 16:54:21 -0500

    starting to add files

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//Express app for Flashcards
+
+const express = require("express");
+const cors = require("cors");
+
+const { NotFoundError } = require("./expressError");
+
+const { authenticateJWT } = require("./middleware/auth");
+const authRoutes = require("./routes/auth");
---
+++ config.js
@@ -0,0 +1,36 @@
+"use strict";
+
+//Shared config for application; can be required many places.
+
+require("dotenv").config();
+require("colors");
+
+const SECRET_KEY = process.env.SECRET_KEY || "flash-those-cards";
+
+const PORT = +process.env.PORT || 3001;
+
+//Use dev db, testing db, or - via env var - production db
+
+function getDatabaseUri() {
+    return process.env.NODE_ENV === "TEST"
+        ? "postgresql://localhost:5432/flashcard_test"
+        : process.env.DATABASE_URL || "postgresql://localhost:5432/flashcard";
+}
+
+// Speed up bcrypt during tests, since the algorithm safety isn't being tested
+
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
+
+console.log("Jobly Config:".green);
+console.log("SECRET_KEY:".yellow, SECRET_KEY);
+console.log("PORT:".yellow, PORT.toString());
+console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
+console.log("Database:".yellow, getDatabaseUri());
+console.log("---");
+
+module.exports = {
+    SECRET_KEY,
+    PORT,
+    BCRYPT_WORK_FACTOR,
+    getDatabaseUri,
+};
