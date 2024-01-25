fc98e2345c80a8e1c8fcc56032f5442926a36359
Annalisa Boerner <annalisa.boerner@gmail.com>
2024-01-25 11:34:22 -0500

    completed app.js

--- app.js
+++ app.js
@@ -9,3 +9,36 @@ const { NotFoundError } = require("./expressError");
 
 const { authenticateJWT } = require("./middleware/auth");
 const authRoutes = require("./routes/auth");
+const decksRoutes = require("./routes/decks");
+
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+//Parses json and makes it avail via req.body
+app.use(express.json());
+//Formats the console output to a tiny version
+app.use(morgan("tiny"));
+app.use(authenticateJWT);
+
+app.use("/auth", authRoutes);
+app.use("/decks", decksRoutes);
+
+//Handle 404 errors: matches everything
+app.use(function (req, res, next) {
+    return next(new NotFoundError());
+});
+
+//Generic error handler: anything not handled goes here
+app.use(function (err, req, res, next) {
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
+
+    return res.status(status).json({
+        error: { message, status },
+    });
+});
+
+module.exports = app;
---
+++ models/deck.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const db = require("../db");
+const { NotFoundError } = require("../expressError");
+const { sqlForPartialUpdate } = require("../helpers/sql");
+
+//Related functions for decks
+class Deck {
+    //todo?
+}
