a1bc55d94ceb564207cdf0303346fe7608a27142
Lindsey Ipson <lindseyipson@Lindseys-MacBook-Air.local>
2024-03-08 18:37:31 -0800

    update flashcards-schema and flashcards-seed to reflect new database schema

--- flashcards-schema.sql
+++ flashcards-schema.sql
@@ -1,16 +1,25 @@
 CREATE TABLE users (
     username VARCHAR(255) PRIMARY KEY,
-    password TEXT NOT NULL,
-    email TEXT NOT NULL,
-    firstName TEXT NOT NULL,
-    lastName TEXT NOT NULL
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL
 );
+
+
+CREATE TABLE decks (
+    name VARCHAR(255),
+    username VARCHAR(255) NOT NULL,
+    PRIMARY KEY (name, username),
+    FOREIGN KEY (username) REFERENCES users(username)
+);
+
 
 CREATE TABLE cards (
     id SERIAL PRIMARY KEY,
     cardfront TEXT NOT NULL,
     cardback TEXT NOT NULL,
-    deckname TEXT NOT NULL,
-    username    TEXT NOT NULL REFERENCES users
+    deckname VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    FOREIGN KEY (deckname, username) REFERENCES decks(name, username)
 );
-
--- flashcards-seed.sql
+++ flashcards-seed.sql
@@ -1,10 +1,22 @@
--- two test users with password "password"
-
+-- Insert test users
 INSERT INTO users (username, password, email, firstName, lastName)
-VALUES ('testuser1', '$2b$12$AZH7virni5jlTTiGgEg4zu3lSvAw68qVEfSIOjJ3RqtbJbdW/Oi5q', 'test@user1.com', 'test', 'user1'),
- ('testuser2', '$2b$12$AZH7virni5jlTTiGgEg4zu3lSvAw68qVEfSIOjJ3RqtbJbdW/Oi5q', 'test@user2.com', 'test', 'user2');
+VALUES 
+    ('testuser1', 'password', 'test@user1.com', 'Test', 'User1'),
+    ('testuser2', 'password', 'test@user2.com', 'Test', 'User2');
 
---two cards per test deck
+-- Insert test decks
+INSERT INTO decks (name, username)
+VALUES 
+    ('testdeck1', 'testuser1'),
+    ('testdeck2', 'testuser2'),
+    ('testdeck3', 'testuser2');
 
- INSERT INTO cards (cardfront, cardback, deckname, username)
- VALUES ('front1', 'back1', 'testdeck1', 'testuser1'), ('front2', 'back2', 'testdeck1', 'testuser1'), ('front1', 'back1', 'testdeck2', 'testuser2'), ('front2', 'back2', 'testdeck2', 'testuser2')
+-- Insert test cards, directly referencing 'deckname' and 'username'
+INSERT INTO cards (cardfront, cardback, deckname, username)
+VALUES 
+    ('front1_deck1', 'back1_deck1', 'testdeck1', 'testuser1'),
+    ('front2_deck1', 'back2_deck1', 'testdeck1', 'testuser1'),
+    ('front1_deck2', 'back1_deck2', 'testdeck2', 'testuser2'),
+    ('front2_deck2', 'back2_deck2', 'testdeck2', 'testuser2'),
+    ('front1_deck3', 'back1_deck3', 'testdeck3', 'testuser2'),
+    ('front2_deck3', 'back2_deck3', 'testdeck3', 'testuser2');
